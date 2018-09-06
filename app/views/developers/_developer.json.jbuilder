18639b2af907659492f93de9474004b370fd3780
Mary <l.skyling@gmail.com>
2018-09-06 16:04:25 -0400

    start

--- app/views/developers/_developer.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! developer, :id, :resume_url, :created_at, :updated_at
-json.url developer_url(developer, format: :json)
--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  resources :employers
-  devise_for :users
-  resources :developers
-  resources :projects
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
